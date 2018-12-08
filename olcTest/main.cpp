1a2b8f911e64c66cb73d946b00491a8cb661ae38
Halalai Mircea <boss.mircea67@yahoo.com>
2018-12-08 18:12:35 +0200

    Each snake segment has its own direction which is copied from the previous one in the vector.

--- olcTest/main.cpp
+++ olcTest/main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 class CMD_Snake : public olcConsoleGameEngine
 {
 public:
-	struct fCartesian {
+	struct Cartesian {
 		float x;
 		float y;
 	};
-	struct iCartesian {
-		int x;
-		int y;
+	struct SnakeSegmentInfo {
+		Cartesian pos;
+		Cartesian direction;
 	};
-
+public:
 	CMD_Snake() { m_sAppName = L"CMD_Snake"; }
 
 	virtual bool OnUserCreate() override
@@ -24,78 +24,95 @@ public:
 		bGameOver = false;
 		mapWidth = ScreenWidth() * 4 / 5;
 		mapHeight = ScreenHeight();
-		mHead = { (float)mapWidth / 2,(float)mapHeight / 2 };
+		mHead = { (float)mapWidth / 2, (float)mapHeight / 2, -1, 0 };
 
-		mSnakeTail.push_back({ (int)mHead.x + 1, (int)mHead.y });
-		mSnakeTail.push_back({ (int)mHead.x + 2, (int)mHead.y });
+		mTail.push_back({ mHead.pos.x + 1, mHead.pos.y , -1, 0 });
+		mTail.push_back({ mHead.pos.x + 2, mHead.pos.y , -1, 0 });
 
 		mFruit = { (float)(rand() % (mapWidth - 2)) + 1, (float)(rand() % (mapHeight - 2) + 1) };
-		mDirection = { 0, 0 };
 		mCurrentScore = 0;
 		return true;
 	}
 
 	void inputHandling()
 	{
-		if (m_keys[VK_UP].bPressed && mDirection.y != 1)
-			mDirection = { 0, -1 };
-		if (m_keys[VK_LEFT].bPressed && mDirection.x != 1)
-			mDirection = { -1, 0 };
-		if (m_keys[VK_RIGHT].bPressed && mDirection.x != -1)
-			mDirection = { 1, 0 };
-		if (m_keys[VK_DOWN].bPressed && mDirection.y != -1)
-			mDirection = { 0, 1 };
+		if (m_keys[VK_UP].bPressed && mHead.direction.y != 1)
+			mHead.direction = { 0, -1 };
+		if (m_keys[VK_LEFT].bPressed && mHead.direction.x != 1)
+			mHead.direction = { -1, 0 };
+		if (m_keys[VK_RIGHT].bPressed && mHead.direction.x != -1)
+			mHead.direction = { 1, 0 };
+		if (m_keys[VK_DOWN].bPressed && mHead.direction.y != -1)
+			mHead.direction = { 0, 1 };
 	}
 
 	virtual bool OnUserUpdate(float fElapsedTime) override
 	{
 		if (!bGameOver)
 		{
-			//Refresh the screen
-			Fill(0, 0, ScreenWidth(), ScreenHeight(), ' ');
+			/////
+			/////Game Logic
+			/////
+			//Copy direction for each tail segment from the previous one
+			/*mTail.at(0).direction.x = mHead.direction.x;
+			mTail.at(0).direction.y = mHead.direction.y;
+			for (size_t i = 0; i <= mTail.size() - 2; i++)
+			{
+				mTail.at(i + 1).direction.x = mTail.at(i).direction.x;
+				mTail.at(i + 1).direction.y = mTail.at(i).direction.y;
+			}*/
+
+			for (size_t i = mTail.size() - 1; i >= 1; i--)
+			{
+				mTail.at(i).direction.x = mTail.at(i - 1).direction.x;
+				mTail.at(i).direction.y = mTail.at(i - 1).direction.y;
+			}
+			mTail.at(0).direction.x = mHead.direction.x;
+			mTail.at(0).direction.y = mHead.direction.y;
+
+			for (size_t i = 0; i < mTail.size(); i++)
+			{
+				mTail.at(i).pos.x += SPEED.x * mTail.at(i).direction.x * fElapsedTime;
+				mTail.at(i).pos.y += SPEED.y * mTail.at(i).direction.y * fElapsedTime;
+			}
 			//Input
 			if (IsFocused())
 				inputHandling();
-			//Game Logic
-			for (size_t i = mSnakeTail.size() - 1; i >= 1; i--)
-			{
-				mSnakeTail.at(i).x = (int)mSnakeTail.at(i - 1).x;
-				mSnakeTail.at(i).y = (int)mSnakeTail.at(i - 1).y;
-			}
-			mSnakeTail.at(0).x = (int)mHead.x;
-			mSnakeTail.at(0).y = (int)mHead.y;
+			mHead.pos.x += SPEED.x * mHead.direction.x * fElapsedTime;
+			mHead.pos.y += SPEED.y * mHead.direction.y * fElapsedTime;
 
-			mHead.x += mSpeedX * mDirection.x * fElapsedTime;
-			mHead.y += mSpeedY * mDirection.y * fElapsedTime;
+
 			//Teleporting to opposite side when attempting to exit boundries
-			if ((int)mHead.x == mapWidth - 1)
-				mHead.x = 1;
-			if ((int)mHead.x == 0)
-				mHead.x = mapWidth - 2;
-			if ((int)mHead.y == 0)
-				mHead.y = mapHeight - 2;
-			if ((int)mHead.y == mapHeight - 1)
-				mHead.y = 1;
-			//Increase the tail size and spawn new fruit
-			if ((int)mHead.x == (int)mFruit.x && (int)mHead.y == (int)mFruit.y)
-			{
-				mSnakeTail.push_back({ -1, -1 });
-				mCurrentScore++;
-				mFruit.x = (float)(rand() % (mapWidth - 2) + 1);
-				mFruit.y = (float)(rand() % (mapHeight - 2) + 1);
-			}
-			//Detecting colision with tail
-			for (iCartesian& tailSegment : mSnakeTail)
-				if ((int)mHead.x == (int)tailSegment.x && (int)mHead.y == (int)tailSegment.y && mCurrentScore != 0)
-					bGameOver = true;
+			//if ((int)mHead.pos.x == mapWidth - 1)
+			//	mHead.pos.x = 1;
+			//if ((int)mHead.pos.x == 0)
+			//	mHead.pos.x = mapWidth - 2;
+			//if ((int)mHead.pos.y == 0)
+			//	mHead.pos.y = mapHeight - 2;
+			//if ((int)mHead.pos.y == mapHeight - 1)
+			//	mHead.pos.y = 1;
+			////Increase the tail size and spawn new fruit
+			//if ((int)mHead.pos.x == (int)mFruit.x && (int)mHead.pos.y == (int)mFruit.y)
+			//{
+			//	mTail.push_back({ -1, -1, 0, 0 });
+			//	mCurrentScore++;
+			//	mFruit.x = (float)(rand() % (mapWidth - 2) + 1);
+			//	mFruit.y = (float)(rand() % (mapHeight - 2) + 1);
+			//}
+			////Detecting colision with tail
+			//for (SnakeSegmentInfo& tailSegment : mTail)
+			//	if (mHead.pos.x == tailSegment.pos.x && mHead.pos.y == tailSegment.pos.y && mCurrentScore != 0)
+			//		bGameOver = true;
+			//Refresh the screen
+			Fill(0, 0, ScreenWidth(), ScreenHeight(), L' ');
 			//Drawing on screen
 			for (int i = 0; i < mapWidth; i++)
 				for (int j = 0; j < mapHeight; j++)
 					if (i == 0 || i == mapWidth - 1 || j == 0 || j == mapHeight - 1)
-						Draw(i, j, 9608, COLOUR::FG_DARK_GREEN);		//Draw the walls
-			Draw(mHead.x, mHead.y, '@', COLOUR::FG_BLUE);
-			for (iCartesian& tailSegment : mSnakeTail)
-				Draw(tailSegment.x, tailSegment.y, 'o', COLOUR::FG_BLUE);
+						Draw(i, j, 9608, COLOUR::FG_DARK_GREEN);
+			Draw((int)mHead.pos.x, (int)mHead.pos.y, '@', COLOUR::FG_BLUE);
+			for (SnakeSegmentInfo& tailSegment : mTail)
+				Draw((int)tailSegment.pos.x, (int)tailSegment.pos.y, 'o', COLOUR::FG_BLUE);
 			Draw(mFruit.x, mFruit.y, '$', COLOUR::FG_RED);
 			DrawString(mapWidth + 1, mapHeight * 1 / 4, L"YOUR SCORE: " + to_wstring(mCurrentScore));
 
@@ -106,14 +123,13 @@ public:
 			DrawString(ScreenWidth() / 4, ScreenHeight() / 2, L"Game Over! Press R to restart", COLOUR::FG_RED);
 			if (m_keys['R'].bPressed)
 			{
-				mDirection = { 0, 0 };
 				bGameOver = false;
 				mCurrentScore = 0;
-				mHead = { (float)mapWidth / 2,(float)mapHeight / 2 };
+				mHead = { (float)mapWidth / 2,(float)mapHeight / 2, -1, 0 };
 				mFruit = { (float)(rand() % (mapWidth - 2)) + 2, (float)(rand() % (mapHeight - 2) + 2) };
-				mSnakeTail.erase(mSnakeTail.begin(), mSnakeTail.end());
-				mSnakeTail.push_back({ (int)mHead.x + 1, (int)mHead.y });
-				mSnakeTail.push_back({ (int)mHead.x + 2, (int)mHead.y });
+				mTail.erase(mTail.begin(), mTail.end());
+				mTail.push_back({ mHead.pos.x + 1, mHead.pos.y , -1, 0 });
+				mTail.push_back({ mHead.pos.x + 2, mHead.pos.y , -1, 0 });
 			}
 		}
 		return true;
@@ -121,9 +137,10 @@ public:
 private:
 	int32_t mapWidth, mapHeight;
 	uint32_t mCurrentScore, mHighScore;
-	fCartesian mHead, mFruit, mDirection;
-	float mSpeedX = 10.0f, mSpeedY = 10.0f;
-	vector<iCartesian> mSnakeTail;
+	Cartesian mFruit;
+	const Cartesian SPEED = { 10.0f, 10.0f };
+	SnakeSegmentInfo mHead;
+	vector<SnakeSegmentInfo> mTail;
 	bool bGameOver;
 };
 
